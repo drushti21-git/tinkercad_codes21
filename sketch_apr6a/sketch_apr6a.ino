ce6976f9b0228f4bb5f8ea9fbb709ef74b5d77f1
drushti21-git <drushtiborkar811@gmail.com>
2026-04-08 04:36:10 -0600

    led code

---
+++ sketch_apr6a/sketch_apr6a.ino
@@ -0,0 +1,61 @@
+#define my_led1 7
+#define my_led2 4
+  #define my_led3 2 
+
+
+   void setup() {
+ 
+ pinMode( my_led1,OUTPUT);
+     pinMode( my_led2 ,OUTPUT);
+     pinMode( my_led3 ,OUTPUT);
+ Serial.begin(9600);
+}
+
+void loop() {
+  
+  
+  digitalWrite(my_led1 ,HIGH);
+  digitalWrite(my_led2 ,LOW);
+  digitalWrite(my_led3 ,LOW);
+  Serial.print("LED1 Turn ON");
+  delay(1000);
+  
+  digitalWrite(my_led1,HIGH);
+ digitalWrite(my_led2  ,LOW);
+   digitalWrite(my_led3 ,LOW);
+  
+  Serial.print("LED1 Turn OFF");
+  delay(1000);
+  
+  digitalWrite(my_led1,LOW);
+  digitalWrite(my_led2,HIGH);
+  digitalWrite(my_led3,LOW);
+  Serial.print("LED2 TURN ON");
+  
+   digitalWrite(my_led1,LOW);
+ digitalWrite(my_led2  ,HIGH);
+   digitalWrite(my_led3 ,LOW);
+  
+  Serial.print("LED2 Turn OFF");
+  delay(1000);
+  
+   digitalWrite(my_led1,LOW);
+  digitalWrite(my_led2,LOW);
+  digitalWrite(my_led3,HIGH);
+  Serial.print("LED3 TURN ON");
+  
+   digitalWrite(my_led1,LOW);
+ digitalWrite(my_led2  ,LOW);
+   digitalWrite(my_led3 ,HIGH);
+  
+  Serial.print("LED3 Turn OFF");
+  delay(1000);
+}
+  
+  
+  
+  
+  
+  
+  
+
